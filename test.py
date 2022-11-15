4580e273c58cae0b4c34a520e8bf96fe2b0801e9
Godin <yelena@UMICH.EDU>
2022-11-15 16:24:14 -0500

    update for local video

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import webbrowser
 import serial
-from playsound import playsound
 import pyautogui
 import time
 
-arduino = serial.Serial(port='/dev/tty.usbmodem142101', baudrate = 9600, timeout=0.1)
+arduino = serial.Serial(port='COM3', baudrate = 9600, timeout=0.1)
 cupSet = True
 
 while True:
@@ -13,9 +12,10 @@ while True:
     if read == b'CUP SET\r\n':
         if (cupSet==True):
             cupSet = False
-            webbrowser.open('https://www.youtube.com/watch?v=le6vodQGeVI&t=384s')
+            # webbrowser.open('https://www.youtube.com/watch?v=le6vodQGeVI&t=384s')
             pyautogui.press('space')
         #  playsound('pouring.mp3')
         else: 
             cupSet = True
-            # pyautogui.press('space')    
+            pyautogui.press('space')
+            pyautogui.press('p')
